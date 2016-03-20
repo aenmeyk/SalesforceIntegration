97151d7f520e9bdcf9b724c8670e67e17c1cebad
Kevin Aenmey <kevin.aenmey@live.com>
2016-03-20 09:35:27 -0400

    Configure Salesforce Authentication

--- SalesforceIntegration/Controllers/HomeController.cs
+++ SalesforceIntegration/Controllers/HomeController.cs
@@ -7,14 +7,6 @@ namespace SalesforceIntegration.Controllers
     {
         public ActionResult Index()
         {
-            var oauthUri = "https://login.salesforce.com/services/oauth2/authorize?response_type=code&client_id=%s&redirect_uri=%s&state=%s";
-            var consumerKey = ConfigurationManager.AppSettings["ConsumerKey"];
-            var consumerSecret = ConfigurationManager.AppSettings["ConsumerSecret"];
-            var redirectUri = "https://salesforce-webhook-creator.herokuapp.com/_oauth_callback";
-            var state = "prod";
-
-            ViewBag.LoginUrl = "https://login.salesforce.com/services/oauth2/authorize?response_type=code&client_id=%s&redirect_uri=%s&state=%s";
-
             return View();
         }
 
