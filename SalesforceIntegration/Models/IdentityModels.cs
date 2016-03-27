3d6e678869266487af7a1ea955167654d6aa8a80
Kevin Aenmey <kevin.aenmey@live.com>
2016-03-27 16:20:05 -0400

    Add RefreshToken to Salesforce claims

--- SalesforceIntegration/Models/IdentityModels.cs
+++ SalesforceIntegration/Models/IdentityModels.cs
@@ -1,5 +1,4 @@
-﻿using System.Data.Entity;
-using System.Linq;
+﻿using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -23,10 +22,9 @@ namespace SalesforceIntegration.Models
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await GenerateUserIdentityAsync(manager);
-
-            // Add custom user claims here
             var loginInfo = await authenticationManager.GetExternalLoginInfoAsync();
             var claims = loginInfo.ExternalIdentity.Claims.Where(c => c.Type.StartsWith(SalesforceClaims.BaseUri));
+
             userIdentity.AddClaims(claims);
 
             return userIdentity;
--- SalesforceIntegration/SalesforceClaims.cs
+++ SalesforceIntegration/SalesforceClaims.cs
@@ -4,6 +4,7 @@
     {
         public const string BaseUri = "http://salesforceactionrelaypoc.azurewebsites.net/";
         public const string AccessToken = BaseUri + "AccessToken";
+        public const string RefreshToken = BaseUri + "RefreshToken";
         public const string InstanceUrl = BaseUri + "InstanceUrl";
     }
 }
